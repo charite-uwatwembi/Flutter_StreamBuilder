f75f262fb3166a83578eb6cd9ba902e229688838
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-07-01 12:03:26 +0200

    simple chat app

---
+++ streambuilder/lib/main.dart
@@ -0,0 +1,113 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple Chat App'),
+        ),
+        body: ChatScreen(),
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final StreamController<List<String>> _streamController = StreamController<List<String>>();
+  List<String> _messages = [];
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
+  void _addMessage(String message) {
+    _messages.add(message);
+    _streamController.sink.add(_messages);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: StreamBuilder<List<String>>(
+            stream: _streamController.stream,
+            initialData: [],
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: Text('Awaiting messages...'));
+              }
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index]),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+        MessageInput(onSend: _addMessage),
+      ],
+    );
+  }
+}
+
+class MessageInput extends StatefulWidget {
+  final Function(String) onSend;
+
+  MessageInput({required this.onSend});
+
+  @override
+  _MessageInputState createState() => _MessageInputState();
+}
+
+class _MessageInputState extends State<MessageInput> {
+  final TextEditingController _controller = TextEditingController();
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      widget.onSend(_controller.text);
+      _controller.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: 'Enter your message',
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            onPressed: _sendMessage,
+          ),
+        ],
+      ),
+    );
+  }
+}
